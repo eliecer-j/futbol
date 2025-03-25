9d2fc784dc9cbff9bcdcdb915e5710a6414cbca7
eliecer-j <54568015+eliecer-j@users.noreply.github.com>
2025-03-25 09:48:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -93,6 +93,7 @@ def delete():
         rows = [x['id'] for x in arr.data]
         
         res = data.from_('data_futbol').delete().in_('id', rows).execute()
+        print(len(res.data), 'longitud de datos despues de eliminar')
         if len(res.data) == 0:
             print('data eliminada', res.data)
         else:
