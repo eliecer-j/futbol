5e0502eff217918d2bc4aa152e8661f493ad0e4b
eliecer-j <54568015+eliecer-j@users.noreply.github.com>
2025-06-16 11:12:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def extraer_eventos():
         page = context.new_page()
         
         
-        page.goto("https://streamtp4.com/eventos.html")
+        page.goto("https://stream196tp.com/eventos.html")
         
         
         page.wait_for_load_state("networkidle")
